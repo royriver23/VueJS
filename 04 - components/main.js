7a6a04dba26b9ad8d3965e73f4632ef78d1fea0b
Roy Rivera <roy.fita23@gmail.com>
2017-02-19 22:53:08 -0600

    Slot use and components init

--- 04 - components/main.js
+++ 04 - components/main.js
@@ -1,5 +1,20 @@
 Vue.component('task', {
-    template: '<li>Foobar</li>'
+    template: '<li><slot></slot></li>'
+});
+
+Vue.component('links', {
+    template: '<li v-for="link in data().links"><a href="link.url">link.name</a></li>',
+
+    data() {
+      return {
+        links: [
+          { url: 'http://www.google.com', name: 'Google'},
+          { url: 'http://www.youtube.com', name: 'Youtube'},
+          { url: 'http://www.facebook.com', name: 'Facebook'},
+          { url: 'http://www.linkedin.com', name: 'LinkedIn'}
+        ]
+      };
+    }
 });
 
 new Vue({
